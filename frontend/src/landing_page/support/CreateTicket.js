2128d164f6e8f9ee454305d4408f65ad83a85011
Syed Tasavour <syedtasavour86@gmail.com>
2025-01-13 09:06:46 +0530

    Frontend completed, now moving to dashboard frontend

--- frontend/src/landing_page/support/CreateTicket.js
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -1,10 +1,38 @@
 import React from "react";
+import { data } from "../home/features";
+
+const Section = ({  title, ticon,links }) => (
+  <div className="col-4 col-lg-6 col-md-12 mb-5">
+    <h4 className="fs-5">
+    {ticon} <span className="ms-2">{title}</span>
+    </h4>
+    <div className="links">
+      <ul>
+        {links.map((link, index) => (
+          <li key={index} className="list-unstyled">
+            <a href={link.href} className="lh-lg text-decoration-none">
+              {link.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+);
 
 function CreateTicket() {
-    return(
-        <h1>CreateTicket</h1>
-    );
-
+  return (
+    <div className="container">
+      <div className="row p-5">
+        <h1 className="fs-4 fw-light text-left text-muted">
+          To create a ticket, select a relevant topic
+        </h1>
+        {data.map((section, index) => (
+          <Section key={index} ticon={section.ticon} title={section.title} links={section.links} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default CreateTicket;
--- frontend/src/landing_page/support/Hero.js
+++ frontend/src/landing_page/support/Hero.js
@@ -1,10 +1,37 @@
 import React from "react";
 
 function Hero() {
-    return(
-        <h1>Hero</h1>
-    );
+  return (
+    <section className="container-fluid shc">
+      <div className="p-5 pb-0" id="supportWrpper">
+        <h1 className="fs-5">Support Portal</h1>
+        <a href="#" className="text-decoration-none  shc font">
+          Track Tickets
+        </a>
+      </div>
 
+      <div className="row p-5 supportHeroA">
+        <div className="col-6 col-lg-6 col-md-12">
+            <h1 className="fs-4 mb-3">Search for an answer or browse help topics to create a ticket</h1>
+            <input type="text" placeholder="Eg: how do i activate F&O, why is my order getting rejected " /> <br />
+            <a href="" >Track account opening</a> 
+            <a href="" >Track segment activation</a> 
+            <a href="" >Intraday margins </a>
+            <a href="" >Kite user manual</a>
+        </div>
+        <div className="col-6 col-lg-6 col-md-12 mb-4">
+            <h1>Featured</h1>
+            <ol>
+                <li><a href="#">Offer for sale (OFS) - January 2025</a></li>
+                <li><a href="#">Surveillance measure on scrips - January 2025</a></li>
+            </ol>
+        </div>
+      </div>
+
+
+    </section>
+  );
 }
 
 export default Hero;
+
