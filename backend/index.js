6996a3827775951b832dce17049484480df80aa2
Syed Tasavour <syedtasavour86@gmail.com>
2025-01-18 01:31:39 +0530

    Day 9: Started working on backend API

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const express = require("express");
+const { default: mongoose, Mongoose } = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require ("cors");
+
+const { PositionsModel } = require("./models/PositionsModel");
+const { HoldingsModel } = require("./models/HoldingsModel");
+const { OrdersModel } = require("./models/OrdersModel");
+
+const PORT = process.env.PORT || 3002;
+const URI = process.env.MONGO_URL;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.get("/allHoldings", async (req, res) => {
+  let allHoldings = await HoldingsModel.find({});
+  res.json(allHoldings);
+});
+
+app.get("/allPositions", async (req, res) => {
+    let allPositions = await PositionsModel.find({});
+    res.json(allPositions);
+  });
+
+app.listen(PORT, () => {
+  console.log("app started");
+  mongoose.connect(URI);
+  console.log("db connected");
+});
